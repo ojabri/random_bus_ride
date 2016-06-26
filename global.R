3ca58cff10f84833ec7d010d45918c2d51d1ea2a
Hannah Dadds <hannah.dadds@orderdynamics.com>
2016-06-26 18:16:59 +0600

    fixed bug in ridebus. some bus routes are one way, added lgoic to deal with reaching the end of the line in such case

--- global.R
+++ global.R
@@ -8,19 +8,20 @@ distanceStops = function(stop1, stop2, print=T){
  d
 }
 
-ride.bus = function(current.Stop_Code_LBSL, random.walk.max, random.sit.max){
-
-	#current.bus.stop = bus.stops[bus.stops$Stop_Code_LBSL==current.Stop_Code_LBSL,]
-	stops.within.distance = bus.stops[distanceStops(current.Stop_Code_LBSL, bus.stops$Stop_Code_LBSL, F) <= random.walk.max,]
-
-	random.bus.stop = sample(stops.within.distance[, "Stop_Code_LBSL"], 1)
+getRandomStop = function(current.Stop_Code_LBSL, random.walk.max){
+ stops.within.distance = bus.stops[distanceStops(current.Stop_Code_LBSL, bus.stops$Stop_Code_LBSL, F) <= random.walk.max,]
+ random.bus.stop = sample(stops.within.distance[, "Stop_Code_LBSL"], 1) #check if this works when only one stop in radius
 	random.bus.stop.row = subset(bus.stops, Stop_Code_LBSL==random.bus.stop )
-	
-	#cat(paste("Current Bus Stop:", current.Stop_Code_LBSL, "\n"))
+ 
+ #cat(paste("Current Bus Stop:", current.Stop_Code_LBSL, "\n"))
 	#cat(paste("Random Bus Stop:", random.bus.stop, "\n"))
-	distanceStops(current.Stop_Code_LBSL, random.bus.stop, T)
+	distanceStops(current.Stop_Code_LBSL, random.bus.stop, F)
+ 
+ random.bus.stop
+}
 
-	buses.at.stop.index = bus.seq$Stop_Code_LBSL==random.bus.stop
+getRandomSeq = function(random.bus.stop){
+ buses.at.stop.index = bus.seq$Stop_Code_LBSL==random.bus.stop
 	if(sum(buses.at.stop.index)>1){
 		random.route.index = sample(which(buses.at.stop.index), 1)
 		random.route = bus.seq[random.route.index,]
@@ -28,7 +29,13 @@ ride.bus = function(current.Stop_Code_LBSL, random.walk.max, random.sit.max){
 	} else {
 		random.route = bus.seq[buses.at.stop.index,]
 	}
-	
+ random.route
+}
+
+ride.bus = function(current.Stop_Code_LBSL, random.walk.max, random.sit.max){
+ 
+ random.bus.stop = getRandomStop(current.Stop_Code_LBSL, random.walk.max)
+ random.route = getRandomSeq(random.bus.stop)
 	#cat("****\n")
 	#print(random.route)
 	#cat("****\n")
@@ -40,19 +47,28 @@ ride.bus = function(current.Stop_Code_LBSL, random.walk.max, random.sit.max){
 	#cat("****\n")
 	
 	if( max(random.route.seq$Sequence) == random.route$Sequence) {
-		cat("End of line. Switching Direction\n")
+		#cat("End of line. Switching Direction\n")
 		random.route = subset(bus.seq, route==random.route$route & Run==ifelse(random.route$Run=="1", "2", "1") & Sequence==1)
-		random.route.seq = subset(bus.seq, route==random.route$route & Run==Run)
-	
+		random.route.seq = subset(bus.seq, route==random.route$route & Run==random.route$Run)
+  
+  while(nrow(random.route.seq)==0){
+   print("Bus Route has no return bus. Try looking for another bus route at current stop")
+   random.route.new = getRandomSeq(random.bus.stop)
+   
+   #if you get the same route again then try walking to a new bus stop
+   #if(random.route$route == random.route.new$route){
+    #random.bus.stop = getRandomStop(current.Stop_Code_LBSL, random.walk.max)
+    random.route = getRandomSeq(random.bus.stop)
+   #}
+	 	random.route.seq = subset(bus.seq, route==random.route$route & Run==random.route$Run)
+  }
 		# cat("****\n")
 		# print(random.route)
 		# cat("****\n")
-
-		random.route.seq = subset(bus.seq, route==random.route$route & Run==random.route$Run)
 		
-		# cat("****\n")
-		# print(random.route.seq)
-		# cat("****\n")
+		 #cat("****\n")
+		 #print(random.route.seq)
+		 #cat("****\n")
 	
 	}
 	
@@ -76,27 +92,38 @@ random.bus.ride = function(random.walk.max = 1000, random.sit.max = 10){
 	in.central = F
 	init.bus.stop.code = sample(bus.stops$Stop_Code_LBSL, 1)
 	first.bus.stop = subset(bus.stops, Stop_Code_LBSL==init.bus.stop.code)
-	
+ 
 	#print(paste("First stop:", first.bus.stop$Stop_Name, first.bus.stop$Stop_Code_LBSL))
 	}
 	current.bus.code = init.bus.stop.code
 	stop.seq = data.frame()
 	while(!in.central){
-		new.stop = ride.bus(current.bus.code, random.walk.max, random.sit.max)
-		#print(new.stop)
-		#cat(paste("starting at stop:", head(new.stop$Stop_Name,1), head(new.stop$Stop_Code_LBSL,1), ""))
-		stops.in.central = point.in.polygon(new.stop$Location_Easting, new.stop$Location_Northing, centralLondonPostcodes.chull$Eastings, centralLondonPostcodes.chull$Northings)
+  current.bus.stop = subset(bus.stops, Stop_Code_LBSL == current.bus.code)
+ 
+  #simulate bus ride
+  new.stop = ride.bus(current.bus.code, random.walk.max, random.sit.max)
+  #print(new.stop)
+  #cat(paste("starting at stop:", head(new.stop$Stop_Name,1), head(new.stop$Stop_Code_LBSL,1), ""))
+  stops.in.central = point.in.polygon(new.stop$Location_Easting, new.stop$Location_Northing, centralLondonPostcodes.chull$Eastings, centralLondonPostcodes.chull$Northings)
   
-		if(any(stops.in.central==1)){
-			in.central=T
-			n = min(which(stops.in.central==1))
-			cat(paste("n=", n, "\n"))
-		} else {
-			n = nrow(new.stop)
-			current.bus.code = new.stop$Stop_Code_LBSL[n] #tail(new.stop$Stop_Code_LBSL,1)
-		}
-		#cat(paste("ending at stop:", new.stop$Stop_Name[n], new.stop$Stop_Code_LBSL[n], "via", unique(new.stop$route), "\n"))
-		stop.seq = rbind(stop.seq, data.frame(Stop_Code_LBSL = new.stop[c(1:n),"Stop_Code_LBSL"], type = c("start", rep("intermediate", n-2), "end") ))
+  if(any(stops.in.central==1)){
+   #print("******Stop is in central********")
+   in.central=T
+   n = min(which(stops.in.central==1))
+   #cat(paste("n=", n, "\n"))
+  } else {
+   #print("Stop not in central")
+   n = nrow(new.stop)
+   current.bus.code = new.stop$Stop_Code_LBSL[n] #tail(new.stop$Stop_Code_LBSL,1)
+  }
+  
+  
+	 if(n>1){ 
+		 stop.seq = rbind(stop.seq, data.frame(Stop_Code_LBSL = new.stop[c(1:n),"Stop_Code_LBSL"], type = c("start", rep("intermediate", n-2), "end") ))
+		 #cat(paste("ending at stop:", new.stop$Stop_Name[n], new.stop$Stop_Code_LBSL[n], "via", unique(new.stop$route), "\n"))
+  } else {
+   stop.seq = rbind(stop.seq, data.frame(Stop_Code_LBSL = new.stop[1,"Stop_Code_LBSL"], type =  "end" ))
+  }
 	}
 	
 	ride.summary = data.frame(
@@ -104,7 +131,8 @@ random.bus.ride = function(random.walk.max = 1000, random.sit.max = 10){
 		end.stop = new.stop$Stop_Name[n],
 		num.stops = nrow(stop.seq),
 		random.walk.max, 
-		random.sit.max
+		random.sit.max,
+  bus.switches = sum(stop.seq$type=="end")
 	)
 	stops = cbind(order = 1:nrow(stop.seq), stop.seq)
 	
@@ -147,3 +175,4 @@ bus.seq$route = as.character(bus.seq$route)
 #remove stops with strange locations
 bus.stops = subset(bus.stops, Virtual_Bus_Stop==0 & Location_Northing!=999999)
 bus.seq = subset(bus.seq, Virtual_Bus_Stop==0 & Location_Northing!=999999)
+
